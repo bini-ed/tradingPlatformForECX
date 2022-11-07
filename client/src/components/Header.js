9b8446c9fa2871d3e3edf1e71588a863024e871f
bini-ed <69515129+bini-ed@users.noreply.github.com>
2022-11-07 14:41:43 +0300

    Tailwind css configured

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  const navBar = [
+    { name: "Home" },
+    { name: "About" },
+    { name: "What We Do" },
+    { name: "Contact Us" },
+    { name: "Sign Up" },
+  ];
+  return (
+    <div className="flex flex-row bg-[#DEF2E6] justify-between items-center p-[20px]">
+      <h2 className="text-[#074E40]">ECX eTrading</h2>
+      <div className="flex flex-row items-center">
+        {navBar.map((nav, index) => (
+          <div
+            className={`text-[#074E40] mx-2 items-center ${
+              index == 4 ? "bg-green-600 text-white p-2 rounded-lg" : ""
+            }`}
+          >
+            {nav.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
